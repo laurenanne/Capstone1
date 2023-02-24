221eef78e0cb625ea79b0baecb67606582434534
lauren brady <lauren.a.brady@gmail.com>
2023-02-24 13:01:36 -0500

    add os for secret key

--- app.py
+++ app.py
@@ -18,7 +18,7 @@ app.config['SQLALCHEMY_DATABASE_URI'] = (
     os.environ.get('DATABASE_URL', 'postgresql:///harry_potter'))
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 app.config['SQLALCHEMY_ECHO'] = True
-app.config['SECRET_KEY'] = 'secret_key13'
+app.config['SECRET_KEY'] = os.environ.get('SECRET_KEY', 'secretkey13')
 app.config['DEBUG_TB_INTERCEPT_REDIRECTS'] = False
 
 debug = DebugToolbarExtension(app)
